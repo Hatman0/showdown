30d4b58f130170652a5b0cd6a34d99ba60766e41
Hatman0 <70173249+Hatman0@users.noreply.github.com>
2023-08-30 21:04:03 -0400

    Another Update

--- data/mods/Reborn/moves.ts
+++ data/mods/Reborn/moves.ts
@@ -4967,7 +4967,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		basePower: 250,
 		basePowerCallback(source, target, move) {
 			if (this.field.isTerrain('electricterrain')) {
-				if (!source.isAlly(target)) this.hint(`${move.name}'s Explosion is boosted x1.5.`);
+				if (!source.isAlly(target)) this.hint(`${source.species.name}'s Explosion is boosted x1.5.`);
 				return move.basePower * 1.5;
 			}
 			return move.basePower;
@@ -4979,9 +4979,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 		flags: {protect: 1, mirror: 1, noparentalbond: 1},
 		onEffectiveness(typeMod, target, type, move) {
 			if (this.field.isTerrain('electricterrain')) {
-				if (!source.isAlly(target)) this.hint(`${move.name}'s Explosion is electrified.`);
+				if (!source.isAlly(target)) this.hint(`${source.species.name}'s Explosion is electrified.`);
 				return typeMod + this.dex.getEffectiveness('Electric', type);
 			}
+			return typeMod;
 		},
 		selfdestruct: "always",
 		secondary: null,
@@ -9836,12 +9837,49 @@ export const Moves: {[moveid: string]: MoveData} = {
 				}
 			},
 		},
+		
+		terrain: 'electricterrain',
 		secondary: {
-			chance: 100,
-			self: {
-				onHit() {
-					this.field.setTerrain('electricterrain');
-				},
+			duration: 3,
+			durationCallback(source, effect) {
+				if (source?.hasItem('terrainextender')) {
+					return 6;
+				}
+				return 3;
+			},
+			onSetStatus(status, target, source, effect) {
+				if (status.id === 'slp' && target.isGrounded() && !target.isSemiInvulnerable()) {
+					if (effect.id === 'yawn' || (effect.effectType === 'Move' && !effect.secondaries)) {
+						this.add('-activate', target, 'move: Electric Terrain');
+					}
+					return false;
+				}
+			},
+			onTryAddVolatile(status, target) {
+				if (!target.isGrounded() || target.isSemiInvulnerable()) return;
+				if (status.id === 'yawn') {
+					this.add('-activate', target, 'move: Electric Terrain');
+					return null;
+				}
+			},
+			onBasePowerPriority: 6,
+			onBasePower(basePower, attacker, defender, move) {
+				if (move.type === 'Electric' && attacker.isGrounded() && !attacker.isSemiInvulnerable()) {
+					this.debug('electric terrain boost');
+					return this.chainModify([5325, 4096]);
+				}
+			},
+			onFieldStart(field, source, effect) {
+				if (effect?.effectType === 'Ability') {
+					this.add('-fieldstart', 'move: Electric Terrain', '[from] ability: ' + effect.name, '[of] ' + source);
+				} else {
+					this.add('-fieldstart', 'move: Electric Terrain');
+				}
+			},
+			onFieldResidualOrder: 27,
+			onFieldResidualSubOrder: 7,
+			onFieldEnd() {
+				this.add('-fieldend', 'move: Electric Terrain');
 			},
 		},
 		target: "all",
